bcfb375a6d50f1cff0fba869b4033eb9d478a6c2
JurassicBeast <chilerdnokuk@gmail.com>
2024-01-07 01:25:28 +0200

    Version 0.0.4 In the "FindNearestStructureCommand" class: - Added a new method: "chunkHasStructure" (Boolean type), which is going to check if a chunk has a structure inside of it. *** - The mod right now is not functioning as expected.

--- src/main/java/net/findneareststructure/FindNearestStructureCommand.java
+++ src/main/java/net/findneareststructure/FindNearestStructureCommand.java
@@ -1,17 +1,20 @@
 package net.findneareststructure;
 
-import com.google.common.base.Stopwatch;
 import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 import com.mojang.datafixers.util.Pair;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.structure.StructureStart;
 import net.minecraft.text.Text;
-import net.minecraft.util.Util;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.ChunkStatus;
 import net.minecraft.world.gen.structure.Structure;
+
+import java.util.Map;
 
 import static net.minecraft.server.command.CommandManager.literal;
 
@@ -22,18 +25,31 @@ public class FindNearestStructureCommand {
     }
 
     private static int findNearestStructure(CommandContext<ServerCommandSource> context) {
-        Stopwatch stopwatch = Stopwatch.createStarted(Util.TICKER);
-        Pair<BlockPos, RegistryEntry<Structure>> pair = locateStructure(context.getSource().getWorld(), BlockPos.ofFloored(context.getSource().getPosition()), 100);
-        stopwatch.stop();
-        if (pair == null) {
-            return 0;
+        if(chunkHasStructure(context)) {
+            context.getSource().sendFeedback(() -> Text.of("Y"), false);
         }
         return 1;
     }
 
-    public static Pair<BlockPos, RegistryEntry<Structure>> locateStructure(ServerWorld world, BlockPos center, int radius) {
+    private static Pair<BlockPos, RegistryEntry<Structure>> locateNearestStructure(ServerWorld world, BlockPos center, int radius) {
         Pair<BlockPos, RegistryEntry<Structure>> pair = null;
 
         return pair;
+    }
+
+    private static boolean chunkHasStructure(CommandContext<ServerCommandSource> context) {
+        if (context == null) {
+            return false;
+        }
+        ChunkPos chunkpos = context.getSource().getEntity().getChunkPos();
+        Chunk chunk = context.getSource().getWorld().getChunk(chunkpos.x, chunkpos.z, ChunkStatus.STRUCTURE_STARTS);
+        Map<Structure, StructureStart> structure = chunk.getStructureStarts();
+        for (Map.Entry<Structure, StructureStart> entry : structure.entrySet()) {
+            StructureStart structureStart = entry.getValue();
+            if (structureStart != null && structureStart.hasChildren()) {
+                return true;
+            }
+        }
+        return false;
     }
 }
